4d244f16b4dd9c5f889529dfe5d43cb99aed345e
auazze <auazze.music@gmail.com>
2024-10-10 02:13:29 +0500

    next and next

--- learningphp3.php
+++ learningphp3.php
@@ -1,2 +1,25 @@
 <?php
 
+# variadic parameters
+# instead of this
+#
+# function sum(int|float $a, int|float $b): int|float
+# { return $a + $b }
+# echo sum(b: 5, a: 2);   # b is 5 and a is 2 just for knowledge from python, but in php use :
+#
+# do this
+function sum(bool $dumpArr, int|float ...$nums)  # ... - accepting unlimited arguments and must be in the end
+{
+    if ($dumpArr) {
+        var_dump($nums);
+    }
+    return array_sum($nums);
+}
+
+# and now u can add as many args as u want
+printf(sum(false,5, 2, 6, 9, 123));
+
+
+# global variables
+printf("\n--------------------------\n");
+
--- learningphpweb/index.php
+++ learningphpweb/index.php
@@ -1,7 +1,15 @@
-<?php # include_once "example.php";   # error warning - do not stop other parts of script ?>
-<?php # require_once "example.php";   # error fatal - stops script ?>
+<?php # include_once "doesnotexists.php";   # error warning - do not stop other parts of script ?>
+<?php # require_once "doesnotexists.php";   # error fatal - stops script ?>
 
 <?php include_once "nav.php"; ?>
 <?php require_once "nav.php"; ?>
 
+<?php
+
+$data = include "example.php";
+echo $data; # shows 2 from example.php
+
+?>
+
 <h1>Home page</h1>
+
